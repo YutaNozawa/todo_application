6000687979287e34daa5a4e841de3d91f369ef44
DESKTOP-DT8MPH7\hagez <hagezawavita@gmail.com>
2017-05-28 21:04:47 +0900

    5/28 Todoのindexではデフォルトのレイアウトを使わないように設定

--- todo_project/src/Controller/TodoController.php
+++ todo_project/src/Controller/TodoController.php
@@ -14,8 +14,9 @@ class TodoController extends AppController
 {
     public function index()
     {
+        $this->viewBuilder()->setLayout('todo');
     }
-    //エスケープ処理に必要、文字列の中に、文字列を含める場合に使用する
+    //エスケープ処理
     private function h($s)
     {
         return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
