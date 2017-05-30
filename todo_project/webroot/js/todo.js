ee5369d6050660c4bcf26c3d2bee156e78b483e5
DESKTOP-DT8MPH7\hagez <hagezawavita@gmail.com>
2017-05-31 05:23:26 +0900

    チェックボックスを押した際の処理をここに書いていく
    #todosの配下の要素をクリックしたとき、update_todoの要素に対して変更を加える
    実際の処理は現在コメントアウトしているところに書く

---
+++ todo_project/webroot/js/todo.js
@@ -0,0 +1,12 @@
+/**
+ * Created by hagez on 2017/05/30.
+ */
+$(function() {
+    'use strict'
+
+    $('#todos').on('click', '.update_todo', function()) {
+        //idを処理
+
+        //ajaxを処理
+    }
+}
