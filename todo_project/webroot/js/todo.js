7edee020ec1cf45e1bbe43d932e13ec547429eba
DESKTOP-DT8MPH7\hagez <hagezawavita@gmail.com>
2017-06-01 07:33:16 +0900

    チェックボックスが押された際に、クラスを追加する処理とクラスを削除する処理を書きました

--- todo_project/webroot/js/todo.js
+++ todo_project/webroot/js/todo.js
@@ -4,9 +4,22 @@
 $(function() {
     'use strict'
 
-    $('#todos').on('click', '.update_todo', function()) {
-        //idを処理
+    $('#todos').on('click', '.update_todo', function() {
+        //update_todoの親要素、liのデータ属性のdata-idを取得
+        var id = $(this).parents('li').data('id');
 
         //ajaxを処理
-    }
+        //_ajax.phpを読み込む、idを渡す。処理の種類（後々ajax.phpに記載する予定)
+        $.post('_ajax.php', {
+            id: id,
+            mode: 'update'
+            //処理が終わった後の処理
+        }, function(res) {
+            if (res.state === 1) {
+                $('#todo_' + id).find('.todo_title').addClass('done')
+            } else {
+                $('#todo_' + id).find('.todo_title').removeClass('done');
+            }
+        })
+    });
 }
