39a20a4de95f23f4ca424fc49ae0988b878b1e0d
DESKTOP-DT8MPH7\hagez <hagezawavita@gmail.com>
2017-06-01 07:34:18 +0900

    jsファイルで受け取ったデータをjsonのオブジェクト形式に変換して表示
    header()でリダイレクトさせている

--- todo_project/src/Controller/TodoController.php
+++ todo_project/src/Controller/TodoController.php
@@ -18,8 +18,7 @@ class TodoController extends AppController
     {
         try {
             $todos = TableRegistry::get('Todos')->getByAllData();
-
-
+            _ajax();
             $this->set('todos', $todos);
         } catch (Exception $e) {
             echo $e->getMessage();
@@ -30,5 +29,24 @@ class TodoController extends AppController
     private function h($s)
     {
         return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
+    }
+    private function _ajax()
+    {
+        if ($this->request->is('post')) {
+            try {
+
+                $res = $this->request->getData('check1');
+                header('Content-Type: application/json');
+
+                echo json_encode($res);
+                exit;
+
+            } catch (Exception $e) {
+                //例外処理が発生した場合はリダイレクトさせてメッセージを表示させている
+                header($this->request->getServerParams(), ' 500 Internal Server Error', true, 500);
+                echo $e->getMessage();
+                exit;
+            }
+        }
     }
 }
